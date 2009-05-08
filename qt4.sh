056a4430f61d4883a73c7c9d0893bd758368691c
Rex Dieter <rdieter@fedoraproject.org>
2009-05-08 20:37:12 +0000

    remove old crud

--- qt4.sh
+++
@@ -1,9 +0,0 @@
-# Qt4 initialization script (sh)
-
-if [ -z "$QT4DIR" ] ; then
-  QT4DIR=`/usr/bin/pkg-config --variable=prefix Qt`
-  QT4DOCDIR=`/usr/bin/pkg-config --variable=docdir Qt`
-
-  export QT4DIR QT4DOCDIR
-fi
-
